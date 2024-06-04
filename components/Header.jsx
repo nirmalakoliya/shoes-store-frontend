2e10d12b7b7cdd868d6b38ebaa71492dae410368
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-06-04 17:07:25 +0530

    Completed Header Scroll Functionality

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,6 +15,26 @@ const Header = () => {
   const [show, setShow] = useState("translate-y-0");
   const [lastScrollY, setLastScrollY] = useState(0);
 
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("-translate-y-[80px]");
+      } else {
+        setShow("shadow-sm");
+      }
+    } else {
+      setShow("translate-y-0");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
   return (
     <>
       <header
