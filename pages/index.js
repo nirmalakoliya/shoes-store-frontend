a55f2ee12a1a9f96ab8bdeac504b894a22100aad
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-06-05 13:04:53 +0530

    Completed Homepage product listing

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import HeroBanner from "@/components/HeroBanner";
+import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
@@ -19,6 +20,20 @@ export default function Home() {
             </div>
           </div>
           {/* //* heading and paragraph end */}
+
+          {/* //* Product grid start*/}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+          {/* //* Product grid  end*/}
         </Wrapper>
       </main>
     </>
