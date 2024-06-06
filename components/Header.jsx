de2a82f4de633883245b338db41a464f180d3510
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-06-06 09:27:44 +0530

    change css in Header page

--- components/Header.jsx
+++ components/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
 
           <div className="flex items-center gap-2 text-black">
             {/* //* Icon start */}
-            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex  justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
               <IoMdHeartEmpty className="text-[19px] md:text-[24px]" />
               <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
                 61
@@ -76,7 +76,7 @@ const Header = () => {
             {/* //* Icon end */}
 
             {/* //* Mobile icon start */}
-            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -m-2">
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -m-2">
               {mobileMenu ? (
                 <VscChromeClose
                   className="text-[16px]"
