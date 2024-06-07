064c6a88e415ce0ba8e97afaf38fd4eb30cc3e68
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-06-07 10:56:31 +0530

    Delete dummy data and Fetching strapi data in Homepage product

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,8 @@ import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 import { fetchDataFromApi } from "@/utils/api";
-import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  const fetchProducts = async () => {
-    const { data } = await fetchDataFromApi("/api/products");
-    setData(data);
-  };
-
+export default function Home({ products }) {
   return (
     <>
       <main>
@@ -35,6 +24,10 @@ export default function Home() {
 
           {/* //* Product grid start*/}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+            {products?.data?.map((product) => (
+              <ProductCard key={product?.id} data={product} />
+            ))}
+            {/* <ProductCard />
             <ProductCard />
             <ProductCard />
             <ProductCard />
@@ -42,12 +35,19 @@ export default function Home() {
             <ProductCard />
             <ProductCard />
             <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            <ProductCard /> */}
           </div>
           {/* //* Product grid  end*/}
         </Wrapper>
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+    props: { products },
+  };
 }
