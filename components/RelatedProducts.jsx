6c2ef64f80c8d4058411b74016dde1e7dc44d408
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-06-08 11:50:57 +0530

    Completed Product DetailsPage Data Fetching & Mapping

--- components/RelatedProducts.jsx
+++ components/RelatedProducts.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import ProductCard from "./ProductCard";
 
-const RelatedProducts = () => {
+const RelatedProducts = ({ products }) => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -20,26 +21,18 @@ const RelatedProducts = () => {
   };
 
   return (
-    <>
-      <div className="mt-[50px] md:mt-[100px] md:mb-0">
-        <div className="text-2xl font-bold mb-5">You Might Alos Like</div>
-        <Carousel
-          responsive={responsive}
-          containerClass="-mx-[10px]"
-          itemClass="px-[10px]"
-        >
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-        </Carousel>
-      </div>
-    </>
+    <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0">
+      <div className="text-2xl font-bold mb-5">You Might Also Like</div>
+      <Carousel
+        responsive={responsive}
+        containerClass="-mx-[10px]"
+        itemClass="px-[10px]"
+      >
+        {products?.data?.map((product) => (
+          <ProductCard key={product?.id} data={product} />
+        ))}
+      </Carousel>
+    </div>
   );
 };
 
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,137 +1,187 @@
+import { useState } from "react";
+import { IoMdHeartEmpty } from "react-icons/io";
+import Wrapper from "@/components/Wrapper";
 import ProductDetailsCarousel from "@/components/ProductDetailsCarousel";
 import RelatedProducts from "@/components/RelatedProducts";
-import Wrapper from "@/components/Wrapper";
-import React from "react";
-import { IoMdHeartEmpty } from "react-icons/io";
+import { fetchDataFromApi } from "@/utils/api";
+import { getDiscountedPricePercentage } from "@/utils/helper";
+import ReactMarkdown from "react-markdown";
 
-const ProductDetails = () => (
-  <>
-    <div className="w-full md:py-20">
-      <Wrapper>
-        <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
-          <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:max-0">
-            <ProductDetailsCarousel />
+const ProductDetails = ({ product, products }) => {
+  const [selectedSize, setSelectedSize] = useState();
+  const [showError, setShowError] = useState(false);
+  const p = product?.data?.[0]?.attributes;
+
+  return (
+    <>
+      <div className="w-full md:py-20">
+        <Wrapper>
+          <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
+            {/* left column start */}
+            <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:max-0">
+              <ProductDetailsCarousel images={p.image.data} />
+            </div>
+            <div className="flex-[1] py-3">
+              {/* //* Title */}
+              <div className="text-[34px] font-semibold mb-2 leading-tight">
+                {p.name}
+              </div>
+
+              {/* //* Subtitle */}
+              <div className="text-lg font-semibold mb-5">{p.subtitle}</div>
+
+              {/* //* Product Price  */}
+              <div className="flex items-center">
+                <p className="mr-2 text-lg font-semibold">
+                  MRP : &#8377;{p.price}
+                </p>
+                {p.original_price && (
+                  <>
+                    <p className="text-base  font-medium line-through">
+                      &#8377;{p.original_price}
+                    </p>
+                    <p className="ml-auto text-base font-medium text-green-500">
+                      {getDiscountedPricePercentage(p.original_price, p.price)}%
+                      off
+                    </p>
+                  </>
+                )}
+              </div>
+
+              <div className="text-md font-medium text-black/[0.5]">
+                incl. of taxes
+              </div>
+
+              <div className="text-md font-medium text-black/[0.5] mb-20">
+                {`(Also includs all applicable duties)`}
+              </div>
+
+              {/* //* Product Size  */}
+              <div className="mb-10">
+                <div className="flex justify-between mb-2">
+                  <div className="text-md font-semibold">Select Size</div>
+                  <div className="text-md font-medium text-black/[0.5] cursor-pointer">
+                    Select Guide
+                  </div>
+                </div>
+
+                {/* HEADING END */}
+
+                {/* SIZE START */}
+                <div id="sizesGrid" className="grid grid-cols-3 gap-2">
+                  {p.size.data.map((item, i) => (
+                    <div
+                      key={i}
+                      className={`border rounded-md text-center py-3 font-medium ${
+                        item.enabled
+                          ? "hover:border-black cursor-pointer"
+                          : "cursor-not-allowed bg-black/[0.1] opacity-50"
+                      } ${selectedSize === item.size ? "border-black" : ""}`}
+                      onClick={() => {
+                        setSelectedSize(item.size);
+                        setShowError(false);
+                      }}
+                    >
+                      {item.size}
+                    </div>
+                  ))}
+                </div>
+                {/* SIZE END */}
+
+                {/* SHOW ERROR START */}
+
+                {showError && (
+                  <div className="text-red-600 mt-1">
+                    Size selection is required
+                  </div>
+                )}
+                {/* SHOW ERROR END */}
+              </div>
+
+              {/* PRODUCT SIZE RANGE END */}
+
+              {/* ADD TO CART BUTTON START */}
+              <button
+                className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75"
+                onClick={() => {
+                  if (!selectedSize) {
+                    setShowError(true);
+                    document.getElementById("sizesGrid").scrollIntoView({
+                      block: "center",
+                      behavior: "smooth",
+                    });
+                  }
+                }}
+              >
+                Add to Cart
+              </button>
+              {/* ADD TO CART BUTTON END */}
+
+              {/* WHISHLIST BUTTON START */}
+              <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
+                Whishlist
+                <IoMdHeartEmpty size={20} />
+              </button>
+              {/* WHISHLIST BUTTON END */}
+
+              <div>
+                <div className="text-lg font-bold mb-5">Product Details</div>
+                <div className="markdown text-md mb-5">
+                  <ReactMarkdown>
+                    {p.description
+                      .map((item) => {
+                        if (
+                          item.type === "paragraph" &&
+                          Array.isArray(item.children)
+                        ) {
+                          return item.children
+                            .map((child) => child.text)
+                            .join("");
+                        }
+                        return "";
+                      })
+                      .join("\n")}
+                  </ReactMarkdown>
+                </div>
+              </div>
+            </div>
+            {/* right column end */}
           </div>
-          <div className="flex-[1] py-3">
-            {/* //* Title */}
-            <div className="text-[34px] font-semibold mb-2">
-              Jordan Retro 6 G
-            </div>
-
-            {/* //* Subtitle */}
-            <div className="text-lg font-semibold mb-5">
-              Men&apos;s Golf Shoes
-            </div>
-
-            {/* //* Product Price  */}
-            <div className="text-lg font-semibold">MRP : ₹ 19 695.00</div>
-
-            <div className="text-md font-medium text-black/[0.5]">
-              incl. of taxes
-            </div>
-
-            <div className="text-md font-medium text-black/[0.5] mb-20">
-              {`(Also includs all applicable duties)`}
-            </div>
-
-            {/* //* Product Size  */}
-            <div className="mb-10">
-              <div className="flex justify-between mb-2">
-                <div className="text-md font-semibold">Select Size</div>
-                <div className="text-md font-medium text-black/[0.5]">
-                  Select Guide
-                </div>
-              </div>
-
-              <div className="grid grid-cols-3 gap-2">
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 6
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 6.5
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 7
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 7.5
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 8
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 8.5
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 9
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
-                  UK 9.5
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium cursor-not-allowed bg-black/[0.1] opacity-50">
-                  UK 10
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium cursor-not-allowed bg-black/[0.1] opacity-50">
-                  UK 10.5
-                </div>
-
-                <div className="border rounded-md text-center py-3 font-medium cursor-not-allowed bg-black/[0.1] opacity-50">
-                  UK 11
-                </div>
-              </div>
-
-              <div className="text-red-600 mt-1">
-                Size selection is required
-              </div>
-            </div>
-
-            <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">
-              Add to Cart
-            </button>
-
-            <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
-              Whishlist
-              <IoMdHeartEmpty size={20} />
-            </button>
-
-            <div>
-              <div className="text-lg font-bold mb-5">Product Details</div>
-              <div className="text-md mb-5">
-                "You've got the hops and the speed—lace up in shoes that enhance
-                what you bring to the court. The latest AJ is all about
-                take-offs and landings, with multiple Air units to get you off
-                the ground. The upper is made with strong, reinforced leno-weave
-                fabric that'll keep you contained and leave your game
-                uncompromised. This low-top model is designed for playing on
-                outdoor courts.
-              </div>
-
-              <div className="text-md mb-5">
-                "You've got the hops and the speed—lace up in shoes that enhance
-                what you bring to the court. The latest AJ is all about
-                take-offs and landings, with multiple Air units to get you off
-                the ground. The upper is made with strong, reinforced leno-weave
-                fabric that'll keep you contained and leave your game
-                uncompromised. This low-top model is designed for playing on
-                outdoor courts.
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <RelatedProducts />
-      </Wrapper>
-    </div>
-  </>
-);
+          <RelatedProducts products={products} />
+        </Wrapper>
+      </div>
+    </>
+  );
+};
 
 export default ProductDetails;
+
+export async function getStaticPaths() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+  const paths = products?.data?.map((p) => ({
+    params: {
+      slug: p.attributes.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const product = await fetchDataFromApi(
+    `/api/products?populate=*&filters[slug][$eq]=${slug}`
+  );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][slug][$ne]=${slug}`
+  );
+
+  return {
+    props: {
+      product,
+      products,
+    },
+  };
+}
