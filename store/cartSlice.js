54969f480242c17cb5bd73886ba03bcfa45a2ec9
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-06-08 17:52:48 +0530

    Working on Cart Functionality

--- store/cartSlice.js
+++ store/cartSlice.js
@@ -15,10 +15,28 @@ export const cartSlice = createSlice({
         state.cartItems.push({ ...action.payload, quantity: 1 });
       }
     },
+
+    updateCart: (state, action) => {
+      state.cartItems = state.cartItems.map((p) => {
+        if (p.id === action.payload.id) {
+          if (action.payload.key === "quantity") {
+            p.attributes.price = p.onQuantityPrice * action.payload.val;
+          }
+          return { ...p, [action.payload.key]: action.payload.val };
+        }
+        return p;
+      });
+    },
+
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (p) => p.id !== action.payload.id
+      );
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, updateCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
