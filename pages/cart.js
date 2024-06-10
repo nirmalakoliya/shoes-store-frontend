daef79126d18dc466a90890ccddbb2312a720d8c
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-06-10 13:06:34 +0530

    Completed Stripe Payment Gateway integration & Checkout Functionality

--- pages/cart.js
+++ pages/cart.js
@@ -2,15 +2,39 @@ import CartItem from "@/components/CartItem";
 import Wrapper from "@/components/Wrapper";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
+import { makePaymentRequest } from "@/utils/api";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
 
 const Cart = () => {
+  const [loading, setLoading] = useState(false);
+
   const { cartItems } = useSelector((state) => state.cart);
 
   const subTotal = useMemo(() => {
     return cartItems.reduce((total, val) => total + val.attributes.price, 0);
   }, [cartItems]);
+
+  const handlePayment = async () => {
+    try {
+      setLoading(true);
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest("/api/orders", {
+        products: cartItems,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.stripeSession.id,
+      });
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
 
   return (
     <div className="w-full md:py-20">
@@ -52,8 +76,12 @@ const Cart = () => {
                   </div>
                 </div>
 
-                <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3">
+                <button
+                  className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
+                  onClick={handlePayment}
+                >
                   Checkout
+                  {loading && <img src="/spinner.svg" />}
                 </button>
               </div>
             </div>
